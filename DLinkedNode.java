e162e542552fc2b82bdc316efbc3a46836aed06d
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-08-09 21:59:59 -0400

    Add DLinkedNode

---
+++ DLinkedNode.java
@@ -0,0 +1,11 @@
+class DLinkedNode {
+    int key;
+    int value;
+    DLinkedNode prev;
+    DLinkedNode next;
+
+    DLinkedNode(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
